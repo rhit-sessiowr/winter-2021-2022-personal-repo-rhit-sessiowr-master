cb0950930e4eaf4ed969e43d8c19d74d0588dcf4
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2021-12-05 20:00:04 -0500

    For loops

--- FollowAlongs/Hello Frameworks/public/scripts/main.js
+++ FollowAlongs/Hello Frameworks/public/scripts/main.js
@@ -3,34 +3,41 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Will Sessions
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.counter = 0;
 
 /** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
+rhit.updateCounter = function () {
 };
 
-rhit.ClassName = class {
-	constructor() {
 
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	const buttons = document.querySelectorAll("#counterButtons button"); 
+	// for(let i = 0; i < buttons.length; i++) {
+	// 	const button = buttons[i];
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button);
+	// 	}
+	// }
+
+	for(const button of buttons) {
+		button.onclick = (event) => {
+			console.log(`You pressed`, button);
+		};
+	}
+
+	// buttons.forEach((button) => {
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button);
+	// 	};
+	// });
 };
 
 rhit.main();

54d1f8eee1468d9b52b959b1e23a666a484454ff
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2021-12-05 20:56:51 -0500

    convert JS to use JQuery

--- FollowAlongs/Hello Frameworks/public/scripts/main.js
+++ FollowAlongs/Hello Frameworks/public/scripts/main.js
@@ -13,19 +13,12 @@ var rhit = rhit || {};
 rhit.counter = 0;
 
 /** function and class syntax examples */
-rhit.updateCounter = function (amount, isMult) {
-	if(isMult) {
-		rhit.counter *= amount;
-	} else {
-		rhit.counter += amount;
-	}
-	document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`; 
-};
+
 
 
 rhit.main = function () {
 	console.log("Ready");
-	const buttons = document.querySelectorAll("#counterButtons button"); 
+	// const buttons = document.querySelectorAll("#counterButtons button"); 
 	// for(let i = 0; i < buttons.length; i++) {
 	// 	const button = buttons[i];
 	// 	button.onclick = (event) => {
@@ -33,22 +26,41 @@ rhit.main = function () {
 	// 	}
 	// }
 
-	for(const button of buttons) {
-		button.onclick = (event) => {
-			const dataAmount = parseInt(button.dataset.amount);
-			const dataIsMultiplication = button.dataset.isMultiplication == "true";
+	// for(const button of buttons) {
+	// 	button.onclick = (event) => {
+	// 		const dataAmount = parseInt(button.dataset.amount);
+	// 		const dataIsMultiplication = button.dataset.isMultiplication == "true";
 
-			console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
-			console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
-			rhit.updateCounter(dataAmount, dataIsMultiplication); 
-		};
-	}
+	// 		console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+	// 		console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
+	// 		rhit.updateCounter(dataAmount, dataIsMultiplication); 
+	// 	};
+	// }
 
 	// buttons.forEach((button) => {
 	// 	button.onclick = (event) => {
 	// 		console.log(`You pressed`, button);
 	// 	};
 	// });
+
+	$("#counterButtons button").click((event) => {
+		const dataAmount = $(event.target).data("amount");
+		const dataIsMultiplication = !!$(event.target).data("isMultiplication");
+		console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+		console.log(`Amount: ${typeof(dataAmount)} isMult: ${typeof(dataIsMultiplication)}`);
+		rhit.updateCounter(dataAmount, dataIsMultiplication); 
+
+	});
+};
+
+rhit.updateCounter = function (amount, isMultiplication) {
+	if(isMultiplication) {
+		rhit.counter *= amount;
+	} else {
+		rhit.counter += amount;
+	}
+	// document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`; 
+	$("#counterText").html( `Count = ${rhit.counter}`);
 };
 
 rhit.main();

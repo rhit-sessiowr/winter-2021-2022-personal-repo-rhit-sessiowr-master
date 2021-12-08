8811289b6424908c02303250f37784be5e4ebfa3
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2021-12-07 22:14:25 -0500

    Added button functionality.

--- FollowAlongs/Hello Frameworks/public/scripts/main.js
+++ FollowAlongs/Hello Frameworks/public/scripts/main.js
@@ -18,13 +18,13 @@ rhit.counter = 0;
 
 rhit.main = function () {
 	console.log("Ready");
-	// const buttons = document.querySelectorAll("#counterButtons button"); 
-	// for(let i = 0; i < buttons.length; i++) {
-	// 	const button = buttons[i];
-	// 	button.onclick = (event) => {
-	// 		console.log(`You pressed`, button);
-	// 	}
-	// }
+	const buttons = document.querySelectorAll("#counterButtons button"); 
+	for(let i = 0; i < buttons.length; i++) {
+		const button = buttons[i];
+		button.onclick = (event) => {
+			console.log(`You pressed`, button);
+		}
+	}
 
 	for(const button of buttons) {
 		button.onclick = (event) => {
--- Homeworks/My Favorite Things/public/scripts/main.js
+++ Homeworks/My Favorite Things/public/scripts/main.js
@@ -31,6 +31,42 @@ rhit.counter = 0;
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+    document.querySelector("#decButton").onclick = (event) => {
+         rhit.counter -= 1;
+         rhit.updateView(rhit.counter);
+    }
+    document.querySelector("#incButton").onclick = (event) => {
+        rhit.counter += 1;
+        rhit.updateView(rhit.counter);
+    }
+    document.querySelector("#resetButton").onclick = (event) => {
+        rhit.counter = 0;
+        rhit.updateView(rhit.counter);
+    }
+
+	document.querySelector("#redButton").onclick = (event) => {
+		document.querySelector("#favoriteColorBox").style["background-color"] = "red";
+		document.querySelector("#favoriteColorBox").innerHTML = "Red";
+	}
+
+	document.querySelector("#blueButton").onclick = (event) => {
+		document.querySelector("#favoriteColorBox").style["background-color"] = "blue";
+		document.querySelector("#favoriteColorBox").innerHTML = "Blue";
+	}
+
+	document.querySelector("#greenButton").onclick = (event) => {
+		document.querySelector("#favoriteColorBox").style["background-color"] = "green";
+		document.querySelector("#favoriteColorBox").innerHTML = "Green";
+	}
+
+	document.querySelector("#purpButton").onclick = (event) => {
+		document.querySelector("#favoriteColorBox").style["background-color"] = "purple";
+		document.querySelector("#favoriteColorBox").innerHTML = "Purple";
+	}
 };
 
+rhit.updateView = function(num) {
+    // document.querySelector("#counterText").innerHTML = "Count = " + counter;
+    document.querySelector("#count").innerHTML = `${num}`;
+}
 rhit.main();

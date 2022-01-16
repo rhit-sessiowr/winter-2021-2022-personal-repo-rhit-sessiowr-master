578844f4b67c2408aafcb2866601d9c7230fbdef
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-15 23:28:48 -0500

    JS scaffolding

--- FollowAlongs/MovieQuotesNoAuth/public/scripts/main.js
+++ FollowAlongs/MovieQuotesNoAuth/public/scripts/main.js
@@ -9,28 +9,56 @@
 /** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_MOVIEQUOTES = "MovieQuotes";
+rhit.FB_KEY_QUOTE = "quote";
+rhit.FB_KEY_MOVIE = "movie";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbMovieQuotesManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
-
 	}
+	updateList() {}
+   }   
 
-	methodName() {
+   rhit.MovieQuote = class {
+	   constructor(id, quote, movie) {
+		   this.id = id;
+		   this.quote = quote;
+		   this.movie = movie;
 
+	   }
+   }
+
+   rhit.FbMovieQuotesManager = class {
+	constructor() {
+	  this._documentSnapshots = [];
+	  this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_MOVIEQUOTES);
 	}
-}
-
+	add(quote, movie) {    }
+	beginListening(changeListener) {    }
+	stopListening() {    }
+	// update(id, quote, movie) {    }
+	// delete(id) { }
+	get length() {    }
+	getMovieQuoteAtIndex(index) {    }
+   }
+   
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+
+	if(document.querySelector("#listPage")) {
+		console.log("You are on the list page.");
+		rhit.fbMovieQuotesManager = new rhit.FbMovieQuotesManager();
+		new rhit.ListPageController();
+	}
+
+	if(document.querySelector("#detailPage")) {
+		console.log("You are on the detail page.");
+	}
+
 
 	
 	//Temp code for read and add

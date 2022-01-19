0cdcf07244f7233ac20f1401a7769600d1dc8307
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-19 01:05:54 -0500

    did a bunch of stuff. its 1 am

--- Homeworks/PhotoBucket/public/scripts/main.js
+++ Homeworks/PhotoBucket/public/scripts/main.js
@@ -3,26 +3,82 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Will Sessions
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_PHOTOBUCKET = "PhotoBucket";
+rhit.FB_KEY_IMAGEURL = "imageUrl";
+rhit.FB_KEY_CAPTION = "caption";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
+rhit.fbPhotosManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
-
+		document.querySelector("#submitAddPhoto").onclick = (event) => {
+			const imageUrl = document.querySelector("#inputImageUrl").value;
+			const caption = document.querySelector("#inputCaption").value;
+			rhit.fbPhotosManager.add(imageUrl, caption);
+		}
 	}
 
 	methodName() {
+
+	}
+}
+
+rhit.Photo = class {
+	constructor(id, imageUrl, caption) {
+		this.id = id;
+		this.imageUrl = imageUrl;
+		this.caption = caption;
+	}
+}
+
+rhit.FbPhotosManager = class {
+	constructor() {
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_PHOTOBUCKET);
+	}
+
+	add(imageUrl, caption) {
+		this._ref.add({
+			[rhit.FB_KEY_IMAGEURL]: imageUrl,
+			[rhit.FB_KEY_CAPTION]: caption,
+			[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now()
+		})
+		.then((docRef) => {
+			console.log("Document written with ID: ", docRef.id);
+		})
+		.catch((error) => {
+			console.error("Error adding document: ", error);
+		});
+	
+	}
+
+	beginListening(changeListener) {
+
+	}
+
+	stopListening() {
+
+	}
+
+	// update(id, imageUrl, caption) {
+
+	// }
+
+	// delete(id) {
+
+	// }
+
+	get length() {
+
+	}
+
+	getPhotoAtIndex(index) {
 
 	}
 }
@@ -31,6 +87,30 @@ rhit.ClassName = class {
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	if(document.querySelector("#listPage")) {
+		console.log("This is the list page.");
+		rhit.fbPhotosManager = new rhit.FbPhotosManager();
+		new rhit.ListPageController();
+
+	}
+
+	if(document.querySelector("#detailPage")) {
+		console.log("This is the detail page.");
+
+	}
+
+	//Temp code
+	// const ref = firebase.firestore().collection("PhotoBucket");
+	// ref.onSnapshot((querySnapshot) => {
+	// 	querySnapshot.forEach((doc) => {
+	// 		console.log(doc.data());
+	// 	});
+	// })
+
+	// ref.add({
+	// 	imageUrl: "test",
+	// 	caption: "test"
+	// });
 };
 
 rhit.main();

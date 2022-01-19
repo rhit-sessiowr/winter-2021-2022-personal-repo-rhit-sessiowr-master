080de90b90afce107854590de326cf85a8a06bd0
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-19 02:23:04 -0500

    More stuff done. Give it up for 2 am!

--- Homeworks/PhotoBucket/public/scripts/main.js
+++ Homeworks/PhotoBucket/public/scripts/main.js
@@ -15,16 +15,41 @@ rhit.FB_KEY_CAPTION = "caption";
 rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
 rhit.fbPhotosManager = null;
 
+//From: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
+
 rhit.ListPageController = class {
 	constructor() {
 		document.querySelector("#submitAddPhoto").onclick = (event) => {
 			const imageUrl = document.querySelector("#inputImageUrl").value;
 			const caption = document.querySelector("#inputCaption").value;
 			rhit.fbPhotosManager.add(imageUrl, caption);
+
+			
 		}
+
+		$("#addPhotoModal").on("show.bs.modal", (event) => {
+			document.querySelector("#inputImageUrl").value = "";
+			document.querySelector("#inputCaption").value = "";
+
+		})
+
+		$("#addPhotoModal").on("shown.bs.modal", (event) => {
+			document.querySelector("#inputImageUrl").focus();		
+		})
+
+
+		rhit.fbPhotosManager.beginListening(this.updateList.bind(this));
 	}
 
-	methodName() {
+	
+
+	updateList() {
 
 	}
 }
@@ -41,6 +66,7 @@ rhit.FbPhotosManager = class {
 	constructor() {
 		this._documentSnapshots = [];
 		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_PHOTOBUCKET);
+		this._unsubscribe = null;
 	}
 
 	add(imageUrl, caption) {
@@ -59,11 +85,19 @@ rhit.FbPhotosManager = class {
 	}
 
 	beginListening(changeListener) {
+		this._unsubscribe = this._ref.onSnapshot((querySnapshot) => {
+			this._documentSnapshots = querySnapshot.docs;
+			changeListener();
+
+			// querySnapshot.forEach((doc) => {
+			// 	console.log(doc.data());
+			// });
+		});
 
 	}
 
 	stopListening() {
-
+		this._unsubscribe();
 	}
 
 	// update(id, imageUrl, caption) {
@@ -75,11 +109,15 @@ rhit.FbPhotosManager = class {
 	// }
 
 	get length() {
-
+		return this._documentSnapshots.length;
 	}
 
 	getPhotoAtIndex(index) {
-
+		const docSnapshot  = this._documentSnapshots[index];
+		const photo = new rhit.Photo(
+			docSnapshot.id, docSnapshot.get(FB_KEY_IMAGEURL), docSnapshot.get(FB_KEY_CAPTION)
+		)
+		return photo;
 	}
 }
 

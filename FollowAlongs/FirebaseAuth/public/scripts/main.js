1b17f657da806049d8d846f90e3f11645e72c544
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-22 22:30:58 -0500

    create account and login implementation

--- FollowAlongs/FirebaseAuth/public/scripts/main.js
+++ FollowAlongs/FirebaseAuth/public/scripts/main.js
@@ -1,36 +1,83 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+		  // User is signed in, see docs for a list of available properties
+		  // https://firebase.google.com/docs/reference/js/firebase.User
+		  var uid = user.uid;
+		  // ...
+		  console.log("user is signed in: ", uid);
+		} else {
+			console.log("no user signed in");
+		  // User is signed out
+		  // ...
+		}
+	  });
+
+	const inputEmailEl = document.querySelector("#inputEmail");
+	const inputPasswordEl = document.querySelector("#inputPassword");
+
+	document.querySelector("#signOutButton").onclick = (event) => {
+		console.log("sign out");
+
+
+		firebase.auth().signOut().then(() => {
+			// Sign-out successful.
+			console.log("You are now signed out");
+		  }).catch((error) => {
+			// An error happened.
+			console.log("sign out error");
+		  });
+
+	};
+	document.querySelector("#createAccountButton").onclick = (event) => {
+		console.log(`create an account for email: ${inputEmailEl.value} and password: ${inputPasswordEl.value} `);
+
+
+
+
+		firebase.auth().createUserWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value)
+			.then((userCredential) => {
+				// Signed in 
+				var user = userCredential.user;
+				// ...
+			})
+			.catch((error) => {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log("create account error: ", errorCode, errorMessage);
+				// ..
+			});
+
+	};
+	document.querySelector("#logInButton").onclick = (event) => {
+		console.log(`log in with email: ${inputEmailEl.value} and password: ${inputPasswordEl.value} `);
+
+
+
+		firebase.auth().signInWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value)
+			.then((userCredential) => {
+				// Signed in
+				var user = userCredential.user;
+				// ...
+			})
+			.catch((error) => {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log("existing account login error: ", errorCode, errorMessage);
+
+			});
+
+	};
+
+
+
 };
 
 rhit.main();

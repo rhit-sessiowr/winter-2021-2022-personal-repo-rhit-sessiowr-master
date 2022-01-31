f7d5a97da70f107a44f3fb51436da13591b1c461
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-31 10:22:11 -0500

    Setup stuff

--- Exams/Exam2/public/scripts/main.js
+++ Exams/Exam2/public/scripts/main.js
@@ -1,5 +1,20 @@
+
 /** namespace. */
 var rhit = rhit || {};
+
+rhit.FB_COLLECTION_COUNTERS = "Counters";
+rhit.FB_KEY_NAME = "name";
+rhit.FB_KEY_VALUE = "value";
+rhit.FB_KEY_CREATED = "created";
+rhit.fbCountersManager = null; 
+
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
+
 
 
 

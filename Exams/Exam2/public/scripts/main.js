9a633f5ff4c55b00feba5369d51c564917c4655a
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-31 10:34:18 -0500

    successfully created list page controller and counter manager

--- Exams/Exam2/public/scripts/main.js
+++ Exams/Exam2/public/scripts/main.js
@@ -21,13 +21,64 @@ function htmlToElement(html) {
 
 
 
-rhit.ClassName = class {
+rhit.ListPageController = class {
 	constructor() {
+		console.log("created list page controller");
+	}
+
+	updateList() {
+
+	}
+}
+
+rhit.Counter = class {
+	constructor(id, name) {
+		this.id = id;
+		this.name = name;
+	}
+}
+
+rhit.FbCountersManager = class {
+	constructor() {
+		console.log("created Counters manager");
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_COUNTERS);
+		this._unsubscribe = null;
+	}
+
+	add(name) {
+		console.log(`name: ${name}`);
+	}
+
+	beginListening(changeListener) {
 
 	}
 
-	methodName() {
+	stopListening() {
+		this._unsubscribe();
+	}
 
+	update(value) {
+
+	}
+
+	delete() {
+
+	}
+
+	get length() {
+		return this._documentSnapshots.length;
+	}
+
+	getCounterAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const c = new rhit.Counter(
+			docSnapshot.id,
+			docSnapshot.get(rhit.FB_KEY_NAME),
+			docSnapshot.get(rhit.FB_KEY_VALUE)
+		);
+
+		return c;
 	}
 }
 
@@ -38,6 +89,9 @@ rhit.ClassName = class {
 /* Main */
 rhit.main = function () {
 	console.log("Ready");
+
+	rhit.fbCountersManager = new rhit.FbCountersManager();
+	new rhit.ListPageController();
 };
 
 rhit.main();

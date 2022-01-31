ff92f818263a7a926acb8371b234d1ef936acc63
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-01-31 10:11:14 -0500

    added acutal needed files

---
+++ Exams/Exam2/public/scripts/main.js
@@ -0,0 +1,28 @@
+/** namespace. */
+var rhit = rhit || {};
+
+
+
+
+
+
+rhit.ClassName = class {
+	constructor() {
+
+	}
+
+	methodName() {
+
+	}
+}
+
+
+
+
+
+/* Main */
+rhit.main = function () {
+	console.log("Ready");
+};
+
+rhit.main();

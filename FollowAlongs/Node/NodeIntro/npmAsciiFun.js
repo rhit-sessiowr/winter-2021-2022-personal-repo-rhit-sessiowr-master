d93c559728b76bce25a39d8e8f00d4359fbf90b6
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-06 17:06:40 -0500

    done with figlet

--- FollowAlongs/Node/NodeIntro/npmAsciiFun.js
+++ FollowAlongs/Node/NodeIntro/npmAsciiFun.js
@@ -2,7 +2,7 @@ console.log("TODO: learn how to use NPM");
 
 var figlet = require('figlet');
 
-figlet('Hello World!!', function(err, data) {
+figlet('CSSE280 using NodeJS', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);

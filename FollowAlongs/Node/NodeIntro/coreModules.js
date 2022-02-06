f9eeb6c498df42bd8dbd2b567b26f9793560fe46
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-06 16:37:29 -0500

    path, util and fs

---
+++ FollowAlongs/Node/NodeIntro/coreModules.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
+
+const filesDirectory = path.join(__dirname, "files");
+
+console.clear();
+util.log("Hello");
+util.log(filesDirectory);
+
+fs.writeFile(   path.join(filesDirectory, "test.txt"), "Hello World written from Node.js", (err) => {
+    if(err) {
+        console.log("Error", err);
+        return;
+    }
+});

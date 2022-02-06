2f849dd6786c335b6aa13f2c78ea6a79827c1052
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-06 16:41:08 -0500

    even more

--- FollowAlongs/Node/NodeIntro/coreModules.js
+++ FollowAlongs/Node/NodeIntro/coreModules.js
@@ -8,10 +8,13 @@ console.clear();
 util.log("Hello");
 util.log(filesDirectory);
 
-fs.writeFile(   path.join(filesDirectory, "test.txt"), "Hello World written from Node.js", (err) => {
-    if(err) {
-        console.log("Error", err);
-        return;
-    }
-    console.log("It succeeded, lets go!");
-});
+// fs.writeFile(   path.join(filesDirectory, "test.txt"), "Hello World written from Node.js", (err) => {
+//     if(err) {
+//         console.log("Error", err);
+//         return;
+//     }
+//     console.log("It succeeded, lets go!");
+// });
+
+const fileContents = fs.readFileSync(path.join(filesDirectory, "test.txt"), "utf-8");
+console.log("from File: ", fileContents);

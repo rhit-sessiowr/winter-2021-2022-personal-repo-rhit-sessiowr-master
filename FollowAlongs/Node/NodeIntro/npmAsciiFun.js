b0cdc7bbd32f65904a8c133ee2e5f34707382672
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-06 17:16:39 -0500

    done and done

--- FollowAlongs/Node/NodeIntro/npmAsciiFun.js
+++ FollowAlongs/Node/NodeIntro/npmAsciiFun.js
@@ -1,12 +1,22 @@
 console.log("TODO: learn how to use NPM");
 
-var figlet = require('figlet');
+// var figlet = require('figlet');
 
-figlet('CSSE280 using NodeJS', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
+// figlet('CSSE280 using NodeJS', function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data)
+// });
+
+const imgToAscii = require('ascii-img-canvas-nodejs')
+
+const opts = {}
+
+
+const asciiImgLocal = imgToAscii('files/rose_logo.png', opts)
+asciiImgLocal.then( (asciiImgLocal) => {
+    console.log(asciiImgLocal);
 });

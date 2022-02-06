e0ec85ae42a84bf7dfc9763e82e74268355e9249
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-06 13:43:09 -0500

    moving stuff

---
+++ FollowAlongs/Node/NodeIntro/files/consoleInput.js
@@ -0,0 +1,8 @@
+console.clear();
+
+process.stdout.write("What is your name? ");
+
+process.stdin.on("data", (data) => {
+    process.stdout.write("\nHello " + data.toString().trim() + "!\n");
+    process.exit();
+});

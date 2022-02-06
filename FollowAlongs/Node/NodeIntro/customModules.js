421ac95944a5f80c810a3ecd6cf26e59dbef04cf
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-06 16:57:58 -0500

    custom modules

---
+++ FollowAlongs/Node/NodeIntro/customModules.js
@@ -0,0 +1,7 @@
+const myModule = require("./myOtherFile");
+console.clear();
+console.log("counter: ", myModule.getCounter());
+myModule.inc();
+myModule.inc();
+myModule.inc();
+console.log("counter: ", myModule.getCounter());
---
+++ FollowAlongs/Node/NodeIntro/myOtherFile.js
@@ -0,0 +1,17 @@
+name = "Will Sessions"; 
+
+let counter = 0;
+inc = () => { counter++; };
+dec = () => { counter--; };
+
+getCounter = () => {
+    return counter;
+}
+
+
+module.exports = {
+    name, 
+    inc,
+    dec,
+    getCounter
+}

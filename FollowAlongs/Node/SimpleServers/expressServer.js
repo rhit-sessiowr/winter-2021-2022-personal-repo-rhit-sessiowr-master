d034b3ac3b57f3f3aeb32f0dfb900bcaecae083f
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-07 00:12:28 -0500

    stuff done

--- FollowAlongs/Node/SimpleServers/expressServer.js
+++ FollowAlongs/Node/SimpleServers/expressServer.js
@@ -4,11 +4,30 @@ var app = express();
 app.use('/static', express.static("Public"));
 
 app.get("/hello", function(req, res) {
-    res.send("<h1>Hello Express!</h1>")
+    let name = req.query.name;  
+    let age = req.query.age;
+
+
+
+
+
+    res.send("<h1>Hello "+name+" ! </h1>" + 
+        "You are " + age + " years old.")
 });
 
 app.get("/goodbye", function(req, res) {
     res.send("<h1>Goodbye!</h1>")
 });
+
+app.post("/myPost", function(req,res) {
+    res.send("HTML code. Done via post");
+});
+
+app.get("/users/:username", function(req,res) {
+    let username = req.params.username;
+    res.send("<h1>Profile for "+username+"</h1>");
+});
+
+app.set('views', './views')
 
 app.listen(3000);

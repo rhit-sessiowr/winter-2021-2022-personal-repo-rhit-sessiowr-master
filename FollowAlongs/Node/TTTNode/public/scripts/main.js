0f3838ab236e8d1f8bea2edfe55b5159b09d29ff
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-16 16:38:48 -0500

    a

--- FollowAlongs/Node/TTTNode/public/scripts/main.js
+++ FollowAlongs/Node/TTTNode/public/scripts/main.js
@@ -16,14 +16,14 @@ rhit.PageController = class {
 		this.game = new rhit.Game();
 
 		const squares = document.querySelectorAll(".square");
-		for(const square of squares) {
+		for (const square of squares) {
 			square.onclick = (event) => {
 				const buttonIndex = parseInt(square.dataset.buttonIndex);
 				this.game.pressedButtonAtIndex(buttonIndex);
 				this.updateView();
 			}
 		}
-		document.querySelector("#newGameButton").onclick  = (event) => {
+		document.querySelector("#newGameButton").onclick = (event) => {
 			this.game = new rhit.Game();
 			this.updateView();
 		}
@@ -37,10 +37,32 @@ rhit.PageController = class {
 
 	updateView() {
 		const squares = document.querySelectorAll(".square");
-		squares.forEach((square,index) => {
+		squares.forEach((square, index) => {
 			square.innerHTML = this.game.getMarkAtIndex(index);
 		});
 		document.querySelector("#gameStateText").innerHTML = this.game.state;
+
+
+		if (this.game.isOTurn) {
+			const boardString = this.game.boardString;
+			console.log("computer takes a turn bozo Board: ", boardString);
+
+			// fetch(`/api/getmove/${boardString}`).then((res) => {
+			// 	console.log(res);
+			// 	return res.json();
+			// }).then((data) => {
+			// 	console.log(data);
+			// });
+
+
+			fetch(`/api/getmove/${boardString}`).then(res => res.json()).then(data => {
+				console.log(data.move);
+				this.game.pressedButtonAtIndex(data.move);
+				this.updateView();
+			});
+		}
+
+
 
 	}
 }
@@ -50,14 +72,14 @@ rhit.Game = class {
 	static Mark = {
 		X: "X",
 		O: "O",
-		NONE: " ", 
+		NONE: " ",
 	}
 
 	static State = {
 		X_TURN: "X's Turn",
 		O_TURN: "O's Turn",
 		X_WIN: "X Wins!",
-		O_WIN: "O Wins!", 
+		O_WIN: "O Wins!",
 		TIE: "Tie Game"
 	}
 
@@ -66,21 +88,21 @@ rhit.Game = class {
 	constructor() {
 		this.board = [];
 		this.state = rhit.Game.State.X_TURN;
-		for (let i = 0;  i < 9; i++) {
+		for (let i = 0; i < 9; i++) {
 			this.board.push(rhit.Game.Mark.NONE);
 		}
-		
+
 
 	}
 
 	pressedButtonAtIndex(Index) {
-		if(this.state == rhit.Game.State.X_WIN || this.state == rhit.Game.State.O_WIN || this.state == rhit.Game.State.TIE) { 
+		if (this.state == rhit.Game.State.X_WIN || this.state == rhit.Game.State.O_WIN || this.state == rhit.Game.State.TIE) {
 			return;
 		}
 		if (this.board[Index] != rhit.Game.Mark.NONE) {
 			return;
 		}
-		if(this.state == rhit.Game.State.X_TURN) {
+		if (this.state == rhit.Game.State.X_TURN) {
 			this.board[Index] = rhit.Game.Mark.X;
 			this.state = rhit.Game.State.O_TURN;
 		} else {
@@ -92,12 +114,12 @@ rhit.Game = class {
 	}
 
 	_checkForGameOver() {
-		if(!this.board.includes(rhit.Game.Mark.NONE)) {
+		if (!this.board.includes(rhit.Game.Mark.NONE)) {
 			this.state = rhit.Game.State.TIE;
 		}
 
 		const lines = [];
-		if(!this.board.includes(rhit.Game.Mark.NONE)) {
+		if (!this.board.includes(rhit.Game.Mark.NONE)) {
 			this.state = rhit.Game.State.TIE;
 		}
 		lines.push(this.board[0] + this.board[1] + this.board[2])
@@ -111,8 +133,8 @@ rhit.Game = class {
 		lines.push(this.board[0] + this.board[4] + this.board[8])
 		lines.push(this.board[2] + this.board[4] + this.board[6])
 
-		for(const line of lines) {
-			if(line == "XXX") {
+		for (const line of lines) {
+			if (line == "XXX") {
 				this.state = rhit.Game.State.X_WIN;
 			} else if (line == "OOO") {
 				this.state = rhit.Game.State.O_WIN;
@@ -128,13 +150,29 @@ rhit.Game = class {
 	getState() {
 		return this.state;
 	}
+
+	get isOTurn() {
+		return this.state == rhit.Game.State.O_TURN;
+	}
+
+	get boardString() {
+		let boardString = "";
+		for (let k = 0; k < 9; k++) {
+			if (this.board[k] == rhit.Game.Mark.NONE) {
+				boardString += "-";
+			} else {
+				boardString += this.board[k];
+			}
+		}
+		return boardString;
+	}
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
-	new rhit.PageController();  
+	new rhit.PageController();
 };
 
 rhit.main();

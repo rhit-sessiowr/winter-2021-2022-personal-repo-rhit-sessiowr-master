a0ccacba6f43aac563a33f59b31721dc4291acd0
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-23 08:39:07 -0500

    move done

--- Exams/Exam3/webServer/app.js
+++ Exams/Exam3/webServer/app.js
@@ -43,9 +43,27 @@ function saveToServer(data) {
 app.get("/api/getdays", function (req, res) {
     res.send(data);
     res.end();
-})
+});
 
-
+/**
+ * Move - Sets the index for one of the days of the week
+ *   method:                    PUT
+ *   path:                      /api/move/:fromindex/:toindex
+ *   expected request body:     none
+ *   side effects:              adjusts the order of the array
+ *   example response:          {"days": ["Saturday","Sunday","Friday","Thursday","Wednesday","Tuesday","Monday"],
+ *                               "moved_day": "Sunday", 
+ *                               "from_index": 0,
+ *                               "to_index": 1}
+*/
+app.put("/api/move/:fromindex/:toindex", function (req, res) {
+    let fromIndex = parseInt(req.params.fromindex);
+    let toIndex = parseInt(req.params.toindex);
+    const dayMoved = data.splice(fromIndex, 1)[0]; // Does the splice and returns the day
+    data.splice(toIndex, 0, dayMoved);  // Inserts the day back into the array at the index
+    res.send({"days": data, "moved_day": dayMoved, "from_index": fromIndex, "to_index": toIndex});
+    res.end();
+});
 
 
 app.listen(3000);

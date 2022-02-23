697f1e26d6d92a8ff8e6ded6fbc3f0dc4a410e6f
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-23 08:57:05 -0500

    functions are in there.

--- Exams/Exam3/webServer/public/scripts/main.js
+++ Exams/Exam3/webServer/public/scripts/main.js
@@ -3,11 +3,12 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Will Sessions
  */
 
 /** namespace. */
 var rhit = rhit || {};
+const URL = "http://localhost:3000/api/";
 
 rhit.ShuffleController = class {
 	constructor() {
@@ -15,10 +16,28 @@ rhit.ShuffleController = class {
 			let fromIndex = document.querySelector("#fromIndex").value;
 			let toIndex = document.querySelector("#toIndex").value;
 			console.log(`Move the weekdays! From index: ${fromIndex} to: ${toIndex}`);
+			this.move(fromIndex, toIndex);
 		}
 		document.querySelector("#resetButton").onclick = (event) => {
 			console.log("reset the days. use the setdays api.");
+			this.reset();
 		}
+
+	}
+
+	move(fromIndex, toIndex) {
+		console.log(`I should be moving! fromIndex: ${fromIndex}, toIndex: ${toIndex}`);
+		this.updateView();
+	}
+
+	reset() {
+		console.log("I should be resetting!");
+		this.updateView();
+	}
+
+	updateView() {
+		console.log("Update the View!");
+
 	}
 }
 

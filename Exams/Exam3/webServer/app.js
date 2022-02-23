27123c0d849ec1752f05adcc071e2357c97d2ed5
rhit-sessiowr <92756728+rhit-sessiowr@users.noreply.github.com>
2022-02-23 08:12:35 -0500

    more setup

---
+++ Exams/Exam3/webServer/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+// Setup
+const app = express();
+app.use('/', express.static("public"));
+app.use('/api/', bodyParser.urlencoded({
+    extended: true
+}));
+app.use('/api/', bodyParser.json());
+
+// Data from a file.
+let data = [];
+const serverSideStorage = "../data/db.json";
+fs.readFile(serverSideStorage, function (err, buf) {
+    if (err) {
+        console.log("error: ", err);
+    } else {
+        data = JSON.parse(buf.toString());
+    }
+    console.log("Data read from file.");
+});
+
+function saveToServer(data) {
+    fs.writeFile(serverSideStorage, JSON.stringify(data), function (err, buf) {
+        if (err) {
+            console.log("error: ", err);
+        } else {
+            console.log("Data saved successfully!");
+        }
+    })
+}
